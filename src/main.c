cee86b80899c16f42c23d80f651a86d9177a192f
iameeeom <97613102+iameeeom@users.noreply.github.com>
2022-02-23 16:11:39 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -5,23 +5,27 @@
 #include "clocks_and_modes.h"
 #include "UART.h"
 
-void init_set(){
+void PORT_set(){
 	_4ND_7SEG_init();
 	_1ND_7SEG_init();
-	PORT_init();
+	LEDS_PORT_init();
 	SOSC_init_8MHz();
 	SPLL_init_160MHz();
 	NormalRUNmode_80MHz();
 	UART_PORT_init();
-	}
+}
 
-int main(){
-	init_set();
-
-
-
+void INIT_set(){
 
 }
 
+int main(){
+	// 포트 세팅
+	PORT_set();
 
+	// 초기 설정
+	INIT_set();
 
+	while(1){
+	}
+}
