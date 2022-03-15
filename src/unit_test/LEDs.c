f2c671a317350143240e20c15e31db8da7d09680
Heeseung Eom <97613102+iameeeom@users.noreply.github.com>
2022-03-15 20:51:14 +0900

    Update LEDs.c

--- src/unit_test/LEDs.c
+++ src/unit_test/LEDs.c
@@ -1,63 +1,19 @@
 #include "define.h"
-
-
-void LEDS_PORT_init(){
-	PCC_PORTB |= (1<<CGC);
-
-	PORTB_S1 &= ~((0b111)<<MUX);
-	PORTB_S1 |= (1<<MUX);
-
-	PORTB_S2 &= ~((0b111)<<MUX);
-	PORTB_S2 |= (1<<MUX);
-
-	PORTB_B1 &= ~((0b111)<<MUX);
-	PORTB_B1 |= (1<<MUX);
-
-	PORTB_B2 &= ~((0b111)<<MUX);
-	PORTB_B2 |= (1<<MUX);
-
-	PORTB_B3 &= ~((0b111)<<MUX);
-	PORTB_B3 |= (1<<MUX);
-
-	PORTB_OUT &= ~((0b111)<<MUX);
-	PORTB_OUT |= (1<<MUX);
-
-	PORTB_TURN &= ~((0b111)<<MUX);
-	PORTB_TURN |= (1<<MUX);
-
-	GPIOD_PDDR |= (1<<S1);
-	GPIOD_PDDR |= (1<<S2);
-	GPIOD_PDDR |= (1<<B1);
-	GPIOD_PDDR |= (1<<B2);
-	GPIOD_PDDR |= (1<<B3);
-	GPIOD_PDDR |= (1<<OUT);
-	GPIOD_PDDR |= (1<<TURN);
-
-
-}
-
+#include "LEDs.h"
 
 int main(){
 	LEDS_PORT_init();
-	GPIOD_PSOR |= (1<<B1);
-	GPIOD_PSOR |= (1<<B2);
-	GPIOD_PSOR |= (1<<B3);
-	GPIOD_PSOR |= (1<<TURN);
-
 
 	while(1){
-		int k = 10000;
-		GPIOD_PTOR |= (1<<S1);
-		GPIOD_PTOR |= (1<<S2);
-		GPIOD_PTOR |= (1<<OUT);
-
+		int k = 1000000;
+		GPIOB_PTOR |= (1<<S1);
+		GPIOB_PTOR |= (1<<S2);
+		GPIOB_PTOR |= (1<<OUT);
+		GPIOB_PTOR |= (1<<B1);
+		GPIOB_PTOR |= (1<<B2);
+		GPIOB_PTOR |= (1<<B3);
+		GPIOB_PTOR |= (1<<TURN);
 		while(k--){
-			GPIOD_PTOR |= (1<<B1);
-			GPIOD_PTOR |= (1<<B2);
-			GPIOD_PTOR |= (1<<B3);
-			GPIOD_PTOR |= (1<<TURN);
 		}
-
-
 	}
 }
